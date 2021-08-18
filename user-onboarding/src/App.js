3ef641a41d8069c5b2f37e298b95a01915315f97
Raj Patel <rajpatel@Rajs-MacBook-Pro.local>
2021-08-18 16:26:41 -0400

    Forms MVP

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,36 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
 
 function App() {
+  const initialUsers = [
+    {
+      name: "",
+      email: "",
+      password: "",
+      tosCheck: true,
+    },
+  ];
+  const [users, setUsers] = useState(initialUsers);
+
+  const submit = (user) => {
+    console.groupCollapsed("submit()");
+    console.log(user);
+    setUsers([...users, user]);
+    console.groupEnd();
+  };
+
   return (
     <div className="App">
-     
+      <h1>Create an Account</h1>
+      <Form submit={submit} />
+      {users.map((user, index) => (
+        <div className="user" key={index}>
+          <h2>{user.name}</h2>
+          <a href="#">{user.email}</a>
+        </div>
+      ))}
     </div>
   );
 }
-
 export default App;
